0c25757ed399e03283a1fdb4349c5b320953624c
yoda <yoda@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-22 21:04:10 +0000

    got rid of devLog-Calls, code reformatted
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/nwan_webdavserver/trunk@38490 735d13b6-9817-0410-8766-e36946ffe9aa

--- webdav.php
+++ webdav.php
@@ -15,7 +15,8 @@ require_once 'backends/class.t3WebdavHybridBackend.php';
 
 $webdav = new nwan_webdavserver($CFG);
 
-class nwan_webdavserver {
+class nwan_webdavserver 
+{
 	
 	private $ezcServer;
 	private $ezcPathFactory;
@@ -28,7 +29,8 @@ class nwan_webdavserver {
 	private $CFG;
 	private $base;
 	
-	function __construct($CFG){
+	function __construct($CFG)
+	{
 		
         // init typo3 user, use for authorisation
 		$this->t3Auth = new  t3Auth($CFG);
@@ -41,7 +43,8 @@ class nwan_webdavserver {
         // init base path
 		$this->base = (@$_SERVER["HTTPS"] === "on" ? "https:" : "http:").'//'.$_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME'];
 		$this->ezcPathFactory = new ezcWebdavBasicPathFactory($this->base);		
-		foreach ( $this->ezcServer->configurations as $conf ){
+		foreach ( $this->ezcServer->configurations as $conf )
+		{
 		    $conf->pathFactory = $this->ezcPathFactory;
 		}
 		
@@ -51,7 +54,8 @@ class nwan_webdavserver {
 			$t3path = '/';
 
 		// init memory backend
-		switch(strpos($t3path, 'FILEMOUNTS')>0){
+		switch(strpos($t3path, 'FILEMOUNTS')>0)
+		{
 //			case true:
 //			case '1':
 //				$this->t3Backend = new ezcWebdavFileBackend('/Users/andreas/Sites/dummy-4.3.3/fileadmin');
@@ -64,28 +68,25 @@ class nwan_webdavserver {
 				// get curr dir's contents
 				$this->t3CurrDir = array();
 				$_currDirContents = $this->CFG->t3io->T3ListDir($t3path);
-				$this->CFG->t3io->metaftpd_devlog(5,'$_currDirContents:'.print_r($_currDirContents, true) ,'nwan_webdavserver::__construct($CFG)',"ServeRequest");
 				
 				//turn every leaf into a collection
-				foreach($_currDirContents as $_node ){ 
-					
+				foreach($_currDirContents as $_node )
+				{ 			
 					$this->CFG->t3io->metaftpd_devlog(5,'->$_node:'.$t3path.$_node ,'nwan_webdavserver::__construct($CFG)',"ServeRequest");
 					$this->t3CurrDir[$_node] = $this->CFG->t3io->T3IsDir($t3path.$_node) ? array() :$_node;
 				}
 				
-				$this->CFG->t3io->metaftpd_devlog(5,'$this->t3CurrDir:'.print_r($this->t3CurrDir, true) ,'nwan_webdavserver::__construct($CFG)',"ServeRequest");
-				
 				// append the curr dir to the whole path
-				foreach(array_reverse(explode('/', $t3path)) as $_treenode){
-					
-					if($_treenode != '' && !isset($this->t3CurrDir[$_treenode])){ 		
+				foreach(array_reverse(explode('/', $t3path)) as $_treenode)
+				{		
+					if($_treenode != '' && !isset($this->t3CurrDir[$_treenode]))
+					{ 		
 						$$_treenode = array($_treenode => $this->t3CurrDir);
 						$this->t3CurrDir = $$_treenode;
 					}
 				}
 		
 				// add contents to backend
-				$this->CFG->t3io->metaftpd_devlog(5,'$this->t3CurrDir:'.print_r($_SESSION['t3CurrDir'], true) ,'$this->t3Backend->addContents($this->t3CurrDir)',"ServeRequest");
 				$this->t3Backend->addContents(
 					$this->t3CurrDir
 				);
